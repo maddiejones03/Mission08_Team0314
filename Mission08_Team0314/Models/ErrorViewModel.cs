8ca57412f0cd715a75b1dc82897672a5b194be8e
Maddie Parsons <kaymads@byu.edu>
2026-02-25 15:09:33 -0700

    Initial commit

---
+++ Mission08_Team0314/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission08_Team0314.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
