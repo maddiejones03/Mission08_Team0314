bdbc33e044b37917f4b03c041aa90c18da1ec28c
micdai000 <daimbd000@gmail.com>
2026-02-27 19:03:54 -0700

    Ready for push to main

--- Mission08_Team0314/obj/Debug/net10.0/Mission08_Team0314.AssemblyInfo.cs
+++ Mission08_Team0314/obj/Debug/net10.0/Mission08_Team0314.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission08_Team0314")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a88f17d190a17d3ace8290ef5443cbcaef8a962d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71d8862c4b7f6587bbc56f252c29508eeac34d37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission08_Team0314")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission08_Team0314")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
